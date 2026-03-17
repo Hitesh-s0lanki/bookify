cbfda645a785713962ae83defd4b8590ab2771bf
hitesh-s0lanki-striq <hitesh.solanki@strique.io>
2026-03-17 13:52:37 +0530

    feat: git workflow and pre commit added

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -28,6 +28,7 @@ const navLinks = [
 
 export function Navbar() {
   const [mounted, setMounted] = useState(false);
+  // eslint-disable-next-line react-hooks/set-state-in-effect
   useEffect(() => setMounted(true), []);
 
   return (
